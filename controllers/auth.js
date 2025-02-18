14b4668bd4b1f1ab8173b7de1b017926dc5208ac
EdwardW710 <EdwardW710@yahoo.com>
2025-02-18 11:13:41 -0500

    controllers directory

---
+++ controllers/auth.js
@@ -0,0 +1,6 @@
+const express = require("espress");
+const router = express.Router();
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
+
+const authController = require("./controllers/auth.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -22,6 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+
+app.use('/auth', authController);
 
 // GET /
 app.get("/", async (req, res) => {
